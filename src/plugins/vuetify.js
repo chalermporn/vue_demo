d85280beeebbca66db51630346e844e163a25c9e
BIRD-MAC <chalermporn@aspiron.co.th>
2019-03-20 13:45:45 +0700

    :sparkles: Add Vuetify for App.vue

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  theme: {
+    // primary: '#ee44aa',
+    // secondary: '#424242',
+    // accent: '#82B1FF',
+    // error: '#FF5252',
+    // info: '#2196F3',
+    // success: '#4CAF50',
+    // warning: '#FFC107'
+  },
+  options: {
+    customProperties: true
+  },
+  iconfont: 'mdi' || 'md' || 'fa'
+})
